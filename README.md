c2d2a47d1caed7c4589b1c6eaee502cb8a885022
Thijs Koerselman <thijskoerselman@gmail.com>
2024-09-29 15:43:57 +0200

    Implement things

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # get-or-throw
-A convenience function for adhering to TS noUncheckedIndexedAccess 
+
+A convenience function for adhering to Typescript's `noUncheckedIndexedAccess` setting.
+
+## Installation
+
+```bash
+pnpm add get-or-throw
+```
+
+...or use the equivalent for your package manager.
+
+## Usage
+
+```ts
+const arr = [1, 2, 3];
+console.log(getOrThrow(arr, 1)); // Output: 2
+
+/** This will throw an error: "Index 3 is out of bounds." */
+console.log(getOrThrow(arr, 3));
+
+const obj = { a: 1, b: 2, c: 3 };
+console.log(getOrThrow(obj, "b")); // Output: 2
+
+/** This will throw an error: "Key "d" does not exist in the object." */
+console.log(getOrThrow(obj, "d"));
+```
---
+++ src/get-or-throw.ts
@@ -0,0 +1,32 @@
+/**
+ * Get a value from an object or array, or throw an error if the key or index does not exist.
+ * @param objOrArr The object or array to get the value from.
+ * @param keyOrIndex The key or index to get the value from.
+ * @returns The value at the given key or index.
+ * @throws An error if the key or index does not exist.
+ */
+export function getOrThrow<T extends object, K extends keyof T>(
+  obj: T,
+  key: K
+): T[K];
+export function getOrThrow<T>(arr: T[], index: number): T;
+export function getOrThrow<T extends object, K extends keyof T>(
+  objOrArr: T | T[],
+  keyOrIndex: K | number
+): T[K] | T {
+  if (Array.isArray(objOrArr)) {
+    if (keyOrIndex in objOrArr) {
+      return objOrArr[keyOrIndex as number]!;
+    } else {
+      throw new Error(`Index ${String(keyOrIndex)} is out of bounds.`);
+    }
+  } else {
+    if (keyOrIndex in objOrArr) {
+      return objOrArr[keyOrIndex as K];
+    } else {
+      throw new Error(
+        `Key "${String(keyOrIndex)}" does not exist in the object.`
+      );
+    }
+  }
+}
